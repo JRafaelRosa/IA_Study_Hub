ce7a8910874aabfe6bc1d083ca52f7a81a95b7ae
João Rafael <132322636+nob-boy@users.noreply.github.com>
2025-07-30 10:40:51 -0300

    Criado Artigos e atualizado Categorias

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -19,4 +19,39 @@ class CategoriaController extends Controller
 
         return response()->json($categoria::find($id), 200);
     }
+
+    public function store(Request $request){
+        $regras = [
+            'nome' => 'required',
+            'descricao' => 'required|min:3|max:100'
+        ];
+        $feedback = [
+          'nome.required' => 'O selecione qual é a categoria',
+          'descricao.required' => 'O campo da descrição deve ~ser preenchido',
+          'descricao.min' => 'O campo da descrição deve ter no mínimo 3 caracteres',
+          'descricao.max' => 'O campo da descrição deve ter no máximo 100 caracteres',
+        ];
+
+        $request->validate($regras, $feedback);
+
+        $categoria = Categoria::create($request->all());
+        return response()->json($categoria, 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+
+    }
+
+    public function destroy($id)
+    {
+        $categoria = Categoria::find($id);
+
+        if(!$categoria){
+            return response()->json(['error' => 'Categoria nao encontrada'], 404);
+        }
+
+        $categoria->delete();
+        return response()->json($categoria, 200);
+    }
 }
